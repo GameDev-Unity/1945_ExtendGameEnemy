51e61a37e86ade872ada074f25006fc0bdff89ca
bang0123 <nikolajbang1995@gmail.com>
2017-03-03 13:00:52 +0100

    NYT TRY

---
+++ Assets/scripts/IslandStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IslandStrategy : EnemySpawnerStrategy
+{
+    public override void StartStrategy()
+    {
+        base.StartStrategy();
+    }
+
+    public override void UpdateStrategy()
+    {
+        Debug.Log("Island strategyhehehexd");
+        if (random.Next(1000) < 10)
+        {
+            //instansiate enemy
+            GameObject randomEnemy = (GameObject)Instantiate(Resources.Load("DangerousIsland"));
+            randomEnemy.transform.position = new Vector3(10, random.Next(-4,4), 0);
+
+        }
+    }
+}
+
